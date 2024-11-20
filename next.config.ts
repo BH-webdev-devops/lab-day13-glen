144d1ce694b78045f43b1dd138eea18f2b6a77d2
Glen Lohja <lohjag@MacBook-Pro-von-Glen.local>
2024-11-20 10:19:46 +0100

    test

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  fontLoader: false, // Disable Next.js font optimization
+  experimental: {
+    optimizeCss: true, // This is optional, just for CSS optimizations
+  }
 
 };
 
